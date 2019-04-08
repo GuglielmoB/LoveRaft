672bb11c7be0fcf7a72430d25d2ba720029316c7
Nic Hartley <nxh9052@rit.edu>
2019-04-08 14:44:34 -0400

    Added controls, got XR integrated

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float LookSpeed;
+
+    public bool ForceEnableVerticalLook;
+    public Camera PlayerCamera;
+
+    void Update()
+    {
+        transform.position += Input.GetAxis("Strafe") * transform.right * MoveSpeed;
+        transform.position += Input.GetAxis("Thrust") * transform.forward * MoveSpeed;
+
+        transform.Rotate(transform.up, Input.GetAxis("Camera Horizontal") * LookSpeed);
+        // only rotate the camera itself up/down -- automatically doesn't work if VR is up
+        PlayerCamera.transform.Rotate(-Input.GetAxis("Camera Vertical") * LookSpeed, 0, 0);
+    }
+}

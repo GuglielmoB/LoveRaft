5000129e7789914fcf58a0b02ba842df2264f967
lblair16 <lrb8738@g.rit.edu>
2019-04-15 12:49:56 -0400

    started monster AI

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    //empty game objects used for path positions
+    public List<GameObject> path;
+    public GameObject currentTarget;
+    public GameObject lastTarget;
+
+    //monster movement vals
+    CharacterController characterController;
+    Vector3 acceleration;
+    public Vector3 velocity;
+    public Vector3 position;
+    float startRotation;
+    public float mass = 1f;
+    public float maxSpeed = 0.5f;
+    public float maxTurn = 0.25f;
+    public float radius = 10f;
+
+    //bool to see if monster is aggroed
+    bool aggro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initalize movement vals
+        this.characterController = GetComponent<CharacterController>();
+        velocity = Vector3.zero;
+        acceleration = Vector3.zero;
+        position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        aggro = false;
+
+    }
+
+    protected void CalcSteering()
+    {
+        if (aggro)
+        {
+            //chase the player instead
+        }
+        else
+        {
+            Vector3 pathForce = Seek(currentTarget.transform.position);
+            if (pathForce.sqrMagnitude > maxTurn * maxTurn)
+            {
+                pathForce = pathForce.normalized * maxTurn;
+            }
+            ApplyForce(pathForce);
+        }
+        
+    }
+
+    protected Vector3 Seek(Vector3 targetPos)
+    {
+        Vector3 toTarget = targetPos - this.transform.position;
+        Vector3 desiredVelocity = toTarget.normalized * maxSpeed;
+        Vector3 steeringForce = desiredVelocity - velocity;
+
+        return VectorHelper.Clamp(steeringForce, maxTurn);
+    }
+
+    public void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = transform.position;
+        CalcSteering();
+
+        velocity += acceleration;
+        velocity = VectorHelper.Clamp(velocity, maxSpeed);
+        this.characterController.Move(velocity * Time.deltaTime);
+
+        acceleration = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/VectorHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorHelper : MonoBehaviour {
+
+	public static Vector3 Clamp(Vector3 v, float magLimit) {
+		float mag = v.magnitude;
+		if (mag > magLimit) {
+			v *= magLimit / v.magnitude;
+		}
+		return v;
+	}
+
+	/**
+	 * Computes a vector that is perpindicular to the left of input.
+	 **/
+	public static Vector3 Perpindicularize(Vector3 input) {
+		return new Vector3 (-input.y, input.x, 0);
+	}
+
+	// given an angle in radians
+	// return a unit vector pointing in that direction on the x-z plane
+	public static Vector3 AngleToUnit(float radians) {
+		// hooray trigonometry!
+		return new Vector3 (Mathf.Cos (radians), 0, Mathf.Sin (radians));
+	}
+}

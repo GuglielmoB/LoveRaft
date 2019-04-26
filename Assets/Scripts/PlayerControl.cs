998adde840d973c710e9b16c3c2b0e68d6fa4320
Nic Hartley <nxh9052@rit.edu>
2019-04-26 14:07:51 -0400

    Added sprinting (including configurable exhaustion)

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,8 +5,19 @@ using UnityEngine.XR;
 
 public class PlayerControl : MonoBehaviour
 {
+    [Header("Movement control")]
     public float MoveSpeed;
-    [Header("Camera control options")]
+    public float SprintMultiplier;
+    public float MaxSprint;
+    public float SprintRegenSpeed;
+    public float SprintRegenDelay;
+    private float sprintDelayLeft;
+    private float sprintLeft;
+
+    [Range(0f, 1f)]
+    public float Exhaustion;
+
+    [Header("Camera control")]
     public float LookSpeed;
     [Range(0, 90)]
     public float VerticalLookLimit;
@@ -24,13 +35,35 @@ public class PlayerControl : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         negLookLimit = 360 - VerticalLookLimit;
+        sprintLeft = MaxSprint;
     }
 
     void Update()
     {
         var direction = Quaternion.Euler(0, PlayerCamera.transform.rotation.eulerAngles.y, 0);
-        rb.AddForce(Input.GetAxis("Strafe") * (direction * Vector3.right) * MoveSpeed);
-        rb.AddForce(Input.GetAxis("Thrust") * (direction * Vector3.forward) * MoveSpeed);
+        bool sprintInput = Input.GetButton("Sprint");
+        if (sprintInput)
+        {
+            sprintDelayLeft = SprintRegenDelay;
+            sprintLeft -= Time.deltaTime;
+            if (sprintLeft < 0) sprintLeft = 0;
+        }
+        else
+        {
+            if (sprintDelayLeft <= 0)
+            {
+                sprintLeft += SprintRegenSpeed * Time.deltaTime;
+                if (sprintLeft > MaxSprint) sprintLeft = MaxSprint;
+            }
+            else
+            {
+                sprintDelayLeft -= Time.deltaTime;
+            }
+        }
+        Exhaustion = (MaxSprint - sprintLeft) / MaxSprint;
+        var sprintMult = (sprintInput && sprintLeft > 0) ? SprintMultiplier : 1;
+        rb.AddForce(Input.GetAxis("Strafe") * (direction * Vector3.right) * MoveSpeed * sprintMult);
+        rb.AddForce(Input.GetAxis("Thrust") * (direction * Vector3.forward) * MoveSpeed * sprintMult);
         transform.Rotate(0, Input.GetAxis("Camera Horizontal") * LookSpeed, 0);
 
         if (!XRSettings.enabled)
@@ -53,7 +86,6 @@ public class PlayerControl : MonoBehaviour
         { // move the collider with the camera
             var camPos = PlayerCamera.transform.position - transform.position;
             var workaroundPos = UnityWorkaround.transform.position;
-            Debug.Log($"camPos {camPos}");
             transform.Translate(camPos.x, 0, camPos.z, Space.World);
             UnityWorkaround.transform.position = workaroundPos;
         }
